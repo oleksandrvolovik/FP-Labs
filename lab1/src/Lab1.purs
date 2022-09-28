fcf197104c2173d25f173c2afe46b6e425ed588e
oleksandrvolovik <olieksandr.volovik@gmail.com>
2022-09-28 12:30:06 +0300

    Рекурсивний snoc

--- lab1/src/Lab1.purs
+++ lab1/src/Lab1.purs
@@ -2,7 +2,7 @@ module Lab1 where
 
 import Prelude
 
-import Data.List.Types (List(..), (:))
+import Data.List.Types (List(..))
 import Effect (Effect)
 import Effect.Console (log)
 
@@ -14,20 +14,15 @@ null Nil = true
 null _ = false
 
 snoc :: forall a. List a -> a -> List a
-snoc xs x = reverse $ Cons x $ reverse xs
-
+snoc Nil x = singleton(x)
+snoc (Cons head tail) x = Cons head $ snoc tail x
+ 
 length :: forall a. List a -> Int 
 length a = length' a 0
 
 length' :: forall a. List a -> Int -> Int
 length' Nil totalCount = totalCount
 length' (Cons head tail) currentCount = length' tail (currentCount  + 1)
-
-reverse :: List ~> List
-reverse = go Nil
-  where
-  go acc Nil = acc
-  go acc (x : xs) = go (x : acc) xs
 
 
 test :: Effect Unit
